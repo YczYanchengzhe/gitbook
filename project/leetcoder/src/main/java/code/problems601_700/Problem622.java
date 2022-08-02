a06b261f7a8e5565dc7f58189472e4ed59048e51
yanchengzhe <yanchengzhe@58.com>
2022-08-02 10:16:05 +0800

    leetcoder 622 题

---
+++ project/leetcoder/src/main/java/code/problems601_700/Problem622.java
@@ -0,0 +1,64 @@
+package code.problems601_700;
+
+
+/**
+ * @author chengzhe yan
+ * @description
+ * @date 2022/8/2 09:57
+ */
+public class Problem622 {
+
+	class MyCircularQueue {
+		private int front;
+		private int rear;
+		private int capacity;
+		private int[] elements;
+
+
+		public MyCircularQueue(int k) {
+			capacity = k + 1;
+			elements = new int[capacity];
+			rear = front = 0;
+		}
+
+		public boolean enQueue(int value) {
+			if (isFull()) {
+				return false;
+			}
+			elements[rear] = value;
+			rear = (rear + 1) % capacity;
+			return true;
+		}
+
+		public boolean deQueue() {
+			if (isEmpty()) {
+				return false;
+			}
+			front = (front + 1) % capacity;
+			return true;
+		}
+
+		public int Front() {
+			if (isEmpty()) {
+				return -1;
+			}
+			return elements[front];
+		}
+
+		public int Rear() {
+			if (isEmpty()) {
+				return -1;
+			}
+			return elements[(rear - 1 + capacity) % capacity];
+		}
+
+		public boolean isEmpty() {
+			return rear == front;
+		}
+
+		public boolean isFull() {
+			return (rear + 1) % capacity == front;
+		}
+	}
+
+}
