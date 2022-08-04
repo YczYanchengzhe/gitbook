171496a6f5209e6c7c6df47327680bb4534178d1
yanchengzhe <yanchengzhe@58.com>
2022-08-04 16:36:32 +0800

    leetcoder 1403 题

---
+++ project/leetcoder/src/main/java/code/problems1401_1500/Problem1403.java
@@ -0,0 +1,37 @@
+package code.problems1401_1500;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author chengzhe yan
+ * @description
+ * @date 2022/8/4 16:24
+ */
+public class Problem1403 {
+
+	public static void main(String[] args) {
+		List<Integer> list = new Problem1403().minSubsequence(new int[]{3, 5, 6, 3, 2, 4});
+		System.out.println(list);
+
+	}
+
+	public List<Integer> minSubsequence(int[] nums) {
+		List<Integer> collect = Arrays.stream(nums).boxed().sorted((o1, o2) -> o2 - o1).collect(Collectors.toList());
+		int sum = Arrays.stream(nums).sum();
+		int find = sum / 2;
+		int count = 0;
+		List<Integer> result = new ArrayList<>();
+		for (int num : collect) {
+			count += num;
+			result.add(num);
+			if (count > find) {
+				break;
+			}
+		}
+		return result;
+	}
+}
