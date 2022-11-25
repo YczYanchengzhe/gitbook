1753b7b35cc45bf4137a99da19bcf0d27ef191e7
yanchengzhe <yanchengzhe@58.com>
2022-11-25 09:56:50 +0800

    leetcoder 27 : 双指针问题

---
+++ project/leetcoder/src/main/java/code/problems001_100/Problem27.java
@@ -0,0 +1,34 @@
+package code.problems001_100;
+
+import java.util.Arrays;
+
+/**
+ * @author chengzhe yan
+ * @description
+ * @date 2022/11/24 22:44
+ */
+public class Problem27 {
+
+	public int removeElement(int[] nums, int val) {
+		int length = nums.length;
+		int i = 0;
+		int j = 0;
+		while (i < length) {
+			if (nums[i] != val) {
+				nums[j] = nums[i];
+				j++;
+			}
+			i++;
+		}
+		return j;
+	}
+
+	public static void main(String[] args) {
+		Problem27 problem27 = new Problem27();
+		int[] ints = {0, 1, 2, 2, 3, 0, 4, 2};
+		int i = problem27.removeElement(ints, 2);
+		System.out.println(i);
+		System.out.println(Arrays.toString(ints));
+
+	}
+}
